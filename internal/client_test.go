8504b0b209daec7b865fe823b4497b2f8f3dc512
sidletsky <oleksandr.sidletskyi@gmail.com>
2019-09-21 15:34:57 +0200

    Move test cases for parse() function into client_test.go.
    This will remove html files from the binary.

--- internal/client_test.go
+++ internal/client_test.go
@@ -1,23 +1,15 @@
 package internal
 
 import (
-	"fmt"
 	"io"
-	"os"
 	"reflect"
+	"strings"
 	"testing"
 )
 
 func Test_parse(t *testing.T) {
 	type args struct {
 		r io.Reader
-	}
-	var arguments [4]args
-	for i := 0; i < 4; i++ {
-		path := fmt.Sprintf("client_test/ex%d.html", i+1)
-		file, _ := os.Open(path)
-		arg := args{r: file}
-		arguments[i] = arg
 	}
 	tests := []struct {
 		name    string
@@ -26,22 +18,22 @@ func Test_parse(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "client_test/ex1.html",
-			args:    arguments[0],
+			name:    "ex1.html",
+			args:    args{r: strings.NewReader(ex1)},
 			want:    []Url{{Loc: "/other-page"}},
 			wantErr: false,
 		},
 		{
-			name: "client_test/ex2.html",
-			args: arguments[1],
+			name: "ex2.html",
+			args: args{r: strings.NewReader(ex2)},
 			want: []Url{
 				{Loc: "https://www.twitter.com/joncalhoun"},
 				{Loc: "https://github.com/gophercises"}},
 			wantErr: false,
 		},
 		{
-			name: "client_test/ex3.html",
-			args: arguments[2],
+			name: "ex3.html",
+			args: args{r: strings.NewReader(ex3)},
 			want: []Url{
 				{Loc: "#"},
 				{Loc: "/lost"},
@@ -49,8 +41,8 @@ func Test_parse(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:    "client_test/ex4.html",
-			args:    arguments[3],
+			name:    "ex4.html",
+			args:    args{r: strings.NewReader(ex4)},
 			want:    []Url{{Loc: "/dog-cat"}},
 			wantErr: false,
 		},
@@ -68,3 +60,109 @@ func Test_parse(t *testing.T) {
 		})
 	}
 }
+
+const (
+	ex1 = `<html>
+<body>
+<h1>Hello!</h1>
+<a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
+	ex2 = `<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<h1>Social stuffs</h1>
+<div>
+    <a href="https://www.twitter.com/joncalhoun">
+        Check me out on twitter
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+    </a>
+    <a href="https://github.com/gophercises">
+        Gophercises is on <strong>Github</strong>!
+    </a>
+</div>
+</body>
+</html>
+`
+	ex3 = `<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="ie ie6 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>
+<html class="ie ie7 lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>
+<html class="ie ie9" lang="en"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en" class="no-ie">
+<!--<![endif]-->
+
+<head>
+    <title>Gophercises - Coding exercises for budding gophers</title>
+</head>
+
+<body>
+<section class="header-section">
+    <div class="jumbo-content">
+        <div class="pull-right login-section">
+            Already have an account?
+            <a href="#" class="btn btn-login">Login <i class="fa fa-sign-in" aria-hidden="true"></i></a>
+        </div>
+        <center>
+            <img src="https://gophercises.com/img/gophercises_logo.png" style="max-width: 85%; z-index: 3;">
+            <h1>coding exercises for budding gophers</h1>
+            <br/>
+            <form action="/do-stuff" method="post">
+                <div class="input-group">
+                    <input type="email" id="drip-email" name="fields[email]" class="btn-input"
+                           placeholder="Email Address" required>
+                    <button class="btn btn-success btn-lg" type="submit">Sign me up!</button>
+                    <a href="/lost">Lost? Need help?</a>
+                </div>
+            </form>
+            <p class="disclaimer disclaimer-box">Gophercises is 100% FREE, but is currently in beta. There will be bugs,
+                and things will be changing significantly over the coming weeks.</p>
+        </center>
+    </div>
+</section>
+<section class="footer-section">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-1 vcenter">
+            <div class="quote">
+                "Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all,
+                love of what you are doing or learning to do." - Pele
+            </div>
+        </div>
+        <div class="col-md-4 col-md-offset-0 vcenter">
+            <center>
+                <img src="https://gophercises.com/img/gophercises_lifting.gif" style="width: 80%">
+                <br/>
+                <br/>
+            </center>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <center>
+                <p class="disclaimer">
+                    Artwork created by Marcus Olsson (<a href="https://twitter.com/marcusolsson">@marcusolsson</a>),
+                    animated by Jon Calhoun (that's me!), and inspired by the original Go Gopher created by Renee
+                    French.
+                </p>
+            </center>
+        </div>
+    </div>
+</section>
+</body>
+</html>
+`
+	ex4 = `<html>
+<body>
+<a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
+</body>
+</html>
+`
+)
